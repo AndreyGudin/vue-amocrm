818c2eb5a8fc8c6752b1aca731d733843fb325ba
Andrey Gudin <aeon150791@gmail.com>
2024-07-25 13:33:16 +0400

    feat: add custom list element,pinia store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,11 +1,21 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 
+interface Visitor {
+  id: number;
+  name: string;
+  company: string;
+  group: string;
+  online: boolean;
+}
+
 export const useStore = defineStore("store", () => {
-  const visitors = ref(false);
-  function set(value: boolean) {
-    visitors.value = value;
+  const visitors = ref<Array<Visitor>>([
+    { id: 1, name: "1", company: "2", group: "3", online: true },
+  ]);
+  function add(value: Visitor) {
+    visitors.value.push(value);
   }
 
-  return { visitors, set };
+  return { visitors, add };
 });
