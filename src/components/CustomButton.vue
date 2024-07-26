9117b23b741450bd4f8145e4ea90025fc55f6819
Andrey Gudin <aeon150791@gmail.com>
2024-07-26 11:36:17 +0400

    feat: begin add button

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { useStore } from "@/stores/store";
-import LoaderSvg from "./LoaderSvg.vue";
-
-defineProps<{
-  name: string;
-  loading?: boolean;
-}>();
-const store = useStore();
-
-const chosen =
-  "block px-4 py-2 text-white transition duration-100 ease-in-out bg-blue-500 border border-transparent rounded shadow-sm hover:bg-blue-600 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:bg-white disabled:text-black disabled:font-bold disabled:opacity-50 disabled:cursor-not-allowed";
+withDefaults(
+  defineProps<{
+    name: string;
+    style?: "green" | "gray";
+  }>(),
+  {
+    style: "green",
+  }
+);
 </script>
 
 <template>
-  <button :class="chosen" :disabled="!store.isChosen">
-    <LoaderSvg v-if="loading" />
-    <span v-else>{{ name }}</span>
+  <button class="w-[273px] h-[52px] bg-">
+    <span>{{ name }}</span>
   </button>
 </template>
